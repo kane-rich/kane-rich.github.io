fb9d5213c4abb499ae9e2db2df3ae3af1d84a854
Kane <62842670+kane-rich@users.noreply.github.com>
2022-03-12 12:21:00 +0000

    Psuedo code for Javascript contact form

---
+++ js/contactform.js
@@ -0,0 +1,8 @@
+// Target the contact form popout
+toggle.classlist.'contact-pullout'
+
+// Display the contact form popout using class toggle
+
+// Toggle with event listener onclick for button
+
+// Form submission on the submit button
